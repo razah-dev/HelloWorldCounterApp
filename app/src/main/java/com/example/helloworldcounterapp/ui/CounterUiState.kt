7be745105ee79ce25af6a72de3eda439c2abb882
Mohammad Hussain <razahuss10@gmail.com>
2026-08-21 11:02:58 +0500

    Add View Model (CounterViewModel) to keep track of counter state and also to increment/reset counter state. We also consume any changes to the view model counter state in the screen composable function and make composable functions more reusable.

---
+++ app/src/main/java/com/example/helloworldcounterapp/ui/CounterUiState.kt
@@ -0,0 +1,5 @@
+package com.example.helloworldcounterapp.ui
+
+data class CounterUiState(
+    val counter: Int = 0
+)
---
+++ app/src/main/java/com/example/helloworldcounterapp/ui/CounterViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.helloworldcounterapp.ui
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+private const val LOG_TAG: String = "CounterViewModel"
+
+class CounterViewModel : ViewModel() {
+    private val _uiState: MutableStateFlow<CounterUiState> =
+        MutableStateFlow(CounterUiState())
+    val uiState: StateFlow<CounterUiState> = _uiState.asStateFlow()
+
+    fun incrementCounter() {
+        Log.i(LOG_TAG,  "CounterViewModel incrementCounter() called on ${_uiState.value}")
+        _uiState.update { currentCounterUiState ->
+            currentCounterUiState.copy(
+                counter = currentCounterUiState.counter + 1
+            )
+        }
+    }
+
+    fun resetCounter() {
+        Log.i(LOG_TAG,  "CounterViewModel resetCounter() called on ${_uiState.value}")
+        _uiState.update { currentCounterUiState ->
+            currentCounterUiState.copy(
+                counter = 0
+            )
+        }
+    }
+}
